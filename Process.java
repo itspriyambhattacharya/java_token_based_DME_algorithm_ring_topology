671e6748c79403177a73b8e1e5476ac16b0372bd
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-11 21:08:52 +0530

    Refactor Process class to initialize 'next' attribute in constructor and add setNext method

--- Process.java
+++ Process.java
@@ -2,11 +2,13 @@ public class Process {
     // Attributes
     private int id;
     private boolean token;
+    private Process next;
 
     // Constructor
     public Process(int id) {
         this.id = id;
         this.token = false;
+        this.next = null;
     }
 
     // Methods
@@ -20,5 +22,9 @@ public class Process {
 
     public void setToken(boolean token) {
         this.token = token;
+    }
+
+    public void setNext(Process p1) {
+        this.next = p1;
     }
 }
