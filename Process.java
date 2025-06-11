748d8a69f770ddc6f6d84bc8d835b75e78c2a7b7
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-11 21:15:58 +0530

    Initialize queue attribute in Process class constructor

--- Process.java
+++ Process.java
@@ -1,14 +1,19 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Process {
     // Attributes
     private int id;
     private boolean token;
     private Process next;
+    private Queue queue;
 
     // Constructor
     public Process(int id) {
         this.id = id;
         this.token = false;
         this.next = null;
+        this.queue = new LinkedList<>();
     }
 
     // Methods
