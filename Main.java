94165452e872a0ebeca31f5f838368686c64d34b
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-11 20:53:05 +0530

    Implement process input functionality in Main class

--- Main.java
+++ Main.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("\nEnter number of processes:\t");
+        int n = Integer.parseInt(br.readLine());
+
+        int processWithToken = (int) Math.floor(Math.random() * n);
+
+        List<Process> listOfProcesses = new ArrayList<>(n);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Process " + (i + 1));
+            System.out.print("\nEnter a process id:\t");
+            int pid = Integer.parseInt(br.readLine());
+
+            Process p = new Process(pid);
+            listOfProcesses.add(p);
+        }
+
+        listOfProcesses.get(processWithToken).setToken(true);
+
+        br.close();
     }
 }
