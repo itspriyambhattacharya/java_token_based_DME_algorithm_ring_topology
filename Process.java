ed1030acead19a0060f79d5f6d039641cba78e7e
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2025-06-14 20:14:36 +0530

    Refactor Process class to enhance token request handling and critical section management; improve exit logic and queue processing.

--- Process.java
+++ Process.java
@@ -2,12 +2,11 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Process extends Thread {
-    // Attributes
-    private int id;
-    private boolean hasToken;
-    private Process next;
-    private Queue<Integer> queue;
-    public boolean wantsToEnter;
+    private int id; // Unique Process ID
+    private boolean hasToken; // Whether this process has the token
+    private Process next; // Next process in the ring
+    private Queue<Integer> queue; // Queue to hold pending requests
+    public boolean wantsToEnter; // Whether this process wants to enter CS
 
     // Constructor
     public Process(int id) {
@@ -15,63 +14,120 @@ public class Process extends Thread {
         this.hasToken = false;
         this.next = null;
         this.queue = new LinkedList<>();
-        this.wantsToEnter = true;
+        this.wantsToEnter = true; // Initially every process wants to enter CS
     }
 
-    // Methods
-    public int getProcessId() {
-        return this.id;
+    // Set the next process (ring link)
+    public void setNext(Process nextProcess) {
+        this.next = nextProcess;
     }
 
-    public boolean getToken() {
-        return this.hasToken;
-    }
-
+    // Set token status
     public void setToken(boolean token) {
         this.hasToken = token;
     }
 
-    public void setNext(Process p1) {
-        this.next = p1;
+    // Get process ID
+    public int getProcessId() {
+        return this.id;
     }
 
-    public synchronized void exitCriticalSection() {
-        System.out.println("Process " + id + " exiting critical section.");
-        this.next.hasToken = true; // passing the token to the next connected node
-        while (!queue.isEmpty()) {
-            this.next.queue.offer(this.queue.poll());
-        }
-    }
-
+    // Critical Section Methods
     public synchronized void enterCriticalSection() {
-        System.out.println("Process " + id + " entering critical section...");
+        System.out.println("Process " + id + " is ENTERING critical section...");
         try {
-            Thread.sleep(1000);
+            Thread.sleep(1000); // Simulate work in CS
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         exitCriticalSection();
     }
 
-    public synchronized void requestCriticalSection() {
-        if (wantsToEnter) {
-            if (hasToken) {
-                enterCriticalSection();
-                wantsToEnter = false;
+    public synchronized void exitCriticalSection() {
+        System.out.println("Process " + id + " is EXITING critical section.");
+        this.hasToken = false;
+
+        // Pass token and queue to the next process
+        next.setToken(true);
+        while (!queue.isEmpty()) {
+            int nextRequest = queue.poll();
+            next.queue.offer(nextRequest);
+        }
+        next.checkTokenAndEnterCS(); // Start token handling in next
+    }
+
+    // Token Request Method
+    public synchronized boolean requestToken(int requesterId) {
+        // If this process has the token
+        if (this.hasToken) {
+            if (this.id == requesterId) {
+                // Requester has the token and is itself
+                return true;
+            } else {
+                // Another process is requesting
+                if (!queue.contains(requesterId)) {
+                    queue.offer(requesterId); // Add request to queue
+                }
+
+                // If this process doesn't want to enter CS, pass token
+                if (!this.wantsToEnter) {
+                    this.hasToken = false;
+                    next.setToken(true);
+                    while (!queue.isEmpty()) {
+                        next.queue.offer(queue.poll());
+                    }
+                    next.checkTokenAndEnterCS();
+                }
+
+                return false;
             }
-            hasToken = false;
-            next.setToken(true);
+        } else {
+            // If no token, forward the request to the next process
+            return next.requestToken(requesterId);
         }
     }
 
     @Override
     public void run() {
         try {
-            int rand = (int) Math.floor(Math.random() * 1000 + 1);
-            Thread.sleep(rand); // Small delay to simulate time
+            Thread.sleep((long) (Math.random() * 1000));
+
             requestCriticalSection();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+    }
+
+    public synchronized void requestCriticalSection() {
+        if (wantsToEnter) {
+            boolean granted = requestToken(this.id);
+            if (granted) {
+                enterCriticalSection();
+                wantsToEnter = false;
+            }
+        }
+    }
+
+    // After receiving token, check if should enter CS
+    public synchronized void checkTokenAndEnterCS() {
+        if (this.hasToken) {
+            if (!queue.isEmpty() && queue.peek() == this.id) {
+                queue.poll();
+                enterCriticalSection();
+                wantsToEnter = false;
+            } else if (this.wantsToEnter && queue.isEmpty()) {
+                enterCriticalSection();
+                wantsToEnter = false;
+            } else {
+                // No need to enter CS, pass token
+                this.hasToken = false;
+                next.setToken(true);
+                while (!queue.isEmpty()) {
+                    next.queue.offer(queue.poll());
+                }
+                next.checkTokenAndEnterCS();
+            }
         }
     }
 }
